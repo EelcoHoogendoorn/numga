fixing broadcasting limitation in jax dense operator

numga/backend/jax/operator.py
@@ -132,10 +132,11 @@ class JaxDenseOperator(JaxOperator):
 	@partial(jax.jit, static_argnums=(0,))
 	def __call__(self, *inputs: Tuple[JaxMultiVector]) -> JaxMultiVector:
 		# kernel, shapes = self.precompute
+		shape = jnp.broadcast_shapes(*(i.shape for i in inputs))
 		return self.context.multivector(
 			values=jnp.sum(
-				math.prod((i.values.reshape(s) for i, s in zip(inputs, self.shapes)), start=self.kernel),
-				axis=range(self.arity)
+				math.prod((i.values.reshape(i.shape + s) for i, s in zip(inputs, self.shapes)), start=self.kernel),
+				axis=range(len(shape), len(shape)+self.arity)# FIXME: negative indexing rather than len(shape)?
 			),
 			subspace=self.output
 		)
@@ -167,7 +168,6 @@ class JaxEinsumOperator(JaxOperator):
 
 	@partial(jax.jit, static_argnums=(0,))
 	def __call__(self, *inputs: Tuple[JaxMultiVector]) -> JaxMultiVector:
-		# NOTE: it is assumed here broadcasting is left purely to vmap
 		return self.context.multivector(
 			subspace=self.output,
 			values=jnp.einsum(

numga/examples/physics/run_chain_jax.py
@@ -12,17 +12,17 @@ Body = register(Body)
 Constraint = register(Constraint)
 
 from numga.backend.jax.context import JaxContext as Context
-from numga.backend.jax.operator import JaxEinsumOperator, JaxSparseOperator
+from numga.backend.jax.operator import JaxEinsumOperator, JaxSparseOperator, JaxDenseOperator
 
 
 # want this to work at least for 2d-3d-4d pga/elliptical; still need to add 4d elliptical rendering!
 context = Context(
 	# 'x+y+',
-	'x+y+z+',       # for some reason this is my favorite
+	# 'x+y+z+',       # for some reason this is my favorite
 	# 'x+y+w0',
-	# 'x+y+z+w0',   # should really make the setup a bit more interesting here; not really showing off 3d atm
+	'x+y+z+w0',   # should really make the setup a bit more interesting here; not really showing off 3d atm
+	# otype=JaxDenseOperator,
 	otype=JaxEinsumOperator,
-	# otype=JaxSparseOperator,
 	dtype=jnp.float64
 )
 