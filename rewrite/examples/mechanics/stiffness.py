f4af3bd8660151535989f1ca930e07909cf5c9a8
Eelco Hoogendoorn <>
2026-09-22 09:14:28 +0200

    Refactor point embedding via dual, use 1D array broadcasting, and scalar extension methods

--- rewrite/examples/mechanics/stiffness.py
+++ rewrite/examples/mechanics/stiffness.py
@@ -52,15 +52,8 @@ Inertia = PGA2D.gatype((Wrench, Twist))
 
 
 def point(xy: np.ndarray) -> Point:
-    """Embed (..., 2) Cartesian positions as unit-weight PGA points via basis vectors.
-
-    In PGA, a point (x, y) is the meet (wedge) of coordinate lines:
-    the line X = x (mv.x - x * mv.w) and line Y = y (mv.y - y * mv.w).
-    This constructs points explicitly without depending on blade layout.
-    """
-    xy = np.asarray(xy, dtype=float)
-    x, y = xy[..., 0], xy[..., 1]
-    return (mv.x - x * mv.w) ^ (mv.y - y * mv.w)
+    """Embed (..., 2) Cartesian positions as PGA points (dual of homogeneous vector)."""
+    return (mv.x * xy[..., 0] + mv.y * xy[..., 1] + mv.w).dual()
 
 
 def coordinates(points: Point) -> np.ndarray:
